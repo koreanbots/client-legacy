0b53c3f9d68c2b54089cb17d8658c4da2deeef54
원더 <wonderbotsupport@naver.com>
2020-06-07 09:53:51 +0900

    Fixed https://github.com/koreanbots/koreanbots/issues/63

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import withImportantStyle from 'react-with-important-style';
-import { Container, Image, Segment, Card } from 'semantic-ui-react';
+import { Container, Image, Segment, Card, Icon, Divider } from 'semantic-ui-react';
 
 function About(props) {
   const CustomCard = withImportantStyle('div');
@@ -9,23 +9,30 @@ function About(props) {
     <Container>
       <br/>
       <h1>한국 디스코드봇 리스트</h1>
-      <h2>국내봇을 한곳에서.</h2>
-      <Segment>
+      국내봇을 등록하거나, 확인해볼 수 있는 웹사이트입니다.
+
+      <h2>슬로건</h2>
+      <h3><Icon className="left quote"/>국내봇을 한 곳에서.<Icon className="right quote"/></h3>
+      <Divider/>
       <h2>KOREANBOTS 로고</h2>
+      <Segment>
       로고를 수정하거나, 변경, 왜곡 등 기타 다른 방법으로 로고를 수정하지 말아주세요.
       <Image src="./logo.png"/>
       <Container textAlign="right">
         <a href="/logo.png" download="koreanbots.png">.png</a>
       </Container>
+      <h3>사용된 폰트</h3>
+        <h4>영문: Uni Sans Heavy | 한글: Gugi</h4>
       </Segment>
-
+      <Divider/>
       <h2>KOREANBOTS 색상</h2>
-        <Card.Group stackable>
+        <Card.Group stackabl>
           <CustomCard
             className="ui card"
             style={{
               backgroundColor: '#3366ff !important',
-              padding: '15px !important'
+              padding: '15px !important',
+              color: 'white'
             }}>
               <h2>파랑</h2>
                 <br/>
@@ -35,7 +42,8 @@ function About(props) {
             className="ui card"
             style={{
               backgroundColor: '#1B1C1D !important',
-              padding: '15px !important'
+              padding: '15px !important',
+              color: 'white'
             }}>
               <h2>검정</h2>
                 <br/>
@@ -45,7 +53,8 @@ function About(props) {
             className="ui card"
             style={{
               backgroundColor: '#21BA45 !important',
-              padding: '15px !important'
+              padding: '15px !important',
+              color: 'white'
             }}>
               <h2>초록</h2>
                 <br/>
@@ -55,7 +64,8 @@ function About(props) {
             className="ui card"
             style={{
               backgroundColor: '#DB2828 !important',
-              padding: '15px !important'
+              padding: '15px !important',
+              color: 'white'
             }}>
               <h2>빨강</h2>
                 <br/>
@@ -69,7 +79,8 @@ function About(props) {
             style={{
               backgroundColor: '#7289da !important',
               minwidth: '100%',
-              padding: '15px !important'
+              padding: '15px !important',
+              color: 'white'
             }}>
               <h2>디스코드 보라</h2>
                 <br/>
@@ -79,9 +90,10 @@ function About(props) {
             className="ui card"
             style={{
               backgroundColor: '#FF73FA !important',
-              padding: '15px !important'
+              padding: '15px !important',
+              color: 'white'
             }}>
-              <h2>부스터 분홍색</h2>
+              <h2>부스터 분홍</h2>
                 <br/>
               <h3>rgb(255, 115, 250)<br/>#FF73FA</h3>
           </CustomCard>
