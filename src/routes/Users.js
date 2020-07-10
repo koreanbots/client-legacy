72adf25b41f9b338541f3e278bd0312b1e7a88c3
원더 <wonderbotsupport@naver.com>
2020-07-10 11:28:41 +0900

    Added Github Feature

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -123,6 +123,20 @@ class User extends React.Component {
                     ) : (
                       ''
                     )}
+                    <br/>
+                   
+                    {
+                      data.github ? (
+                    <>
+                      <Label style={{ marginTop: '20px' }} href={`https://github.com/${data.github}`}>
+                      <Icon className="github" />
+                      { data.github }
+                      </Label>
+                    </>
+                        
+                      ) : ("")
+                    }
+                    
                   </Grid.Column>
                 </Grid.Row>
 
@@ -134,7 +148,7 @@ class User extends React.Component {
               </Grid>
               <div>
                 <h2>제작한 봇</h2>
-                <p>해당 유저가 개발에 참여하였거나, 관리하는 봇입니다.</p>
+                <p>해당 유저에 개발하였거나, 관리하는 봇입니다.</p>
                 {data.bots ? (
                   <Card.Group itemsPerRow={3} stackable>
                     {data.bots.map(bot => (
