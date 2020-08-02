9052050d0215536e22578c9f1cfdd5250ba47faf
매리 <mareowst@gmail.com>
2020-08-02 20:03:16 +0900

    fix: production api route

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
   url:
     "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=identify&permissions=0&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fcallback",
 
-  api: "http://localhost:3000" // Does not end with "/"
+  api: "https://api.koreanbots.dev" // Does not end with "/"
 };
