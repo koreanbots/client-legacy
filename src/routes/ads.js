daf551a22f5ec5eb64c35ce28ea918e1532179af
zero734kr <zero734kr@gmail.com>
2020-08-20 22:16:23 -0300

    Update ads.js

--- src/routes/ads.js
+++ src/routes/ads.js
@@ -2,6 +2,7 @@ module.exports = {
     top: {
         img: '/img/attachments/2.png',
         link: 'https://koreanbots.dev'
+    },
     bottom: {
         img: '/img/attachments/2.png',
         link: 'https://koreanbots.dev'
