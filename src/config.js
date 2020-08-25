ab46241b3a0e5cbe21c951554cae718bf7ca5ace
원더 <wonderbotsupport@naver.com>
2020-08-25 09:44:25 +0900

    feat: config for develop

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   url:
-    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=identify&permissions=0&response_type=code&redirect_uri=https%3A%2F%2Fkoreanbots.dev%2Fcallback",
+    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=identify&permissions=0&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fcallback",
 
-  api: "https://api.koreanbots.dev" // Does not end with "/"
-};
+  api: "http://localhost:3000" // Does not end with "/"
+
+}
