bb5dd0f5b4a4c132e42382df3d6ae74cf94f5c0b
Junseo Park <wonderbotsupport@naver.com>
2020-09-03 12:10:50 +0900

    feat: fixed api url

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
   url:
     "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=identify&permissions=0&response_type=code&redirect_uri=https%3A%2F%2Fkoreanbots.dev%2Fcallback",
 
-  api: "https://api.koreanbots.dev" // Does not end with "/"
+  api: "https://api.koreanbots.dev/v1" // Does not end with "/"
 };
