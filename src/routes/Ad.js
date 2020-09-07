7d4b37fda9bc7c5ba128b23a5cd795a1091742b3
원더 <wonderbotsupport@naver.com>
2020-09-07 09:38:31 +0900

    feat: wrap with Article Component & More client info

--- src/routes/Ad.js
+++ src/routes/Ad.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { Container } from 'semantic-ui-react'
+import Article from './Article'
 
 class Ad extends React.Component {
   render() {
     return (
-      <Container>
-        <Helmet>
-          <title>광고 - 한국 디스코드봇 리스트</title>
-          <meta
-            name="description"
-            content="국내 디스코드봇들을 확인하고, 초대해보세요!"
-          />
-        </Helmet>
-        <h1>광고</h1>
+      <Article title="광고" description="광고주분들을 찾고 있습니다.">
         <p>한국 디스코드봇 리스트는 광고주분을 모집합니다.<br/>저희 웹사이트에 광고를 추가하고 싶으시다면 문의해주세요.</p>
         <p>문의: wonderlandpark#9999</p>
- 
-      </Container>
+      </Article>
     )
   }
 }
--- src/routes/Version.js
+++ src/routes/Version.js
@@ -3,14 +3,70 @@ import React from 'react'
 import GitInfo from 'react-git-info/macro'
 import version from '../../package.json'
 
+import Article from './Article'
+
 function Version() {
   return (
-    <div className="loader">
-      <span>
-        [빌드정보] 버전: {version.version} 해시: {GitInfo().commit.hash}
-      </span>
-    </div>
+      <Article title="클라이언트 정보" description="클라이언트 정보들입니다.">
+        <h1>빌드 정보</h1>
+        <p>v{version.version}</p>
+        <p>해시: {GitInfo().commit.hash}</p>
+        <h1>User-Agent</h1>
+        <p>{navigator.userAgent}</p>
+        <h1>Browser</h1>
+        <p>{getBrowser()}</p>
+        <h1>Platform</h1>
+        <p>{navigator.platform}</p>
+        <h1>Language</h1>
+        <p>{navigator.language}</p>
+        <h1>Online?</h1>
+        <p>{navigator.onLine ? 'true' : 'false'}</p>
+      </Article>
   )
 }
 
 export default Version
+
+function getBrowser(){
+  let navUserAgent = navigator.userAgent;
+  let browserName  = navigator.appName;
+  let browserVersion  = ''+parseFloat(navigator.appVersion); 
+  let majorVersion = parseInt(navigator.appVersion,10);
+  let tempNameOffset,tempVersionOffset,tempVersion;
+
+
+if ((tempVersionOffset=navUserAgent.indexOf("Opera"))!=-1) {
+ browserName = "Opera";
+ browserVersion = navUserAgent.substring(tempVersionOffset+6);
+ if ((tempVersionOffset=navUserAgent.indexOf("Version"))!=-1) 
+   browserVersion = navUserAgent.substring(tempVersionOffset+8);
+} else if ((tempVersionOffset=navUserAgent.indexOf("MSIE"))!=-1) {
+ browserName = "Microsoft Internet Explorer";
+ browserVersion = navUserAgent.substring(tempVersionOffset+5);
+} else if ((tempVersionOffset=navUserAgent.indexOf("Chrome"))!=-1) {
+ browserName = "Chrome";
+ browserVersion = navUserAgent.substring(tempVersionOffset+7);
+} else if ((tempVersionOffset=navUserAgent.indexOf("Safari"))!=-1) {
+ browserName = "Safari";
+ browserVersion = navUserAgent.substring(tempVersionOffset+7);
+ if ((tempVersionOffset=navUserAgent.indexOf("Version"))!=-1) 
+   browserVersion = navUserAgent.substring(tempVersionOffset+8);
+} else if ((tempVersionOffset=navUserAgent.indexOf("Firefox"))!=-1) {
+ browserName = "Firefox";
+ browserVersion = navUserAgent.substring(tempVersionOffset+8);
+} else if ( (tempNameOffset=navUserAgent.lastIndexOf(' ')+1) < (tempVersionOffset=navUserAgent.lastIndexOf('/')) ) {
+ browserName = navUserAgent.substring(tempNameOffset,tempVersionOffset);
+ browserVersion = navUserAgent.substring(tempVersionOffset+1);
+ if (browserName.toLowerCase()==browserName.toUpperCase()) {
+  browserName = navigator.appName;
+ }
+}
+
+if ((tempVersion=browserVersion.indexOf(";"))!=-1)
+   browserVersion=browserVersion.substring(0,tempVersion);
+if ((tempVersion=browserVersion.indexOf(" "))!=-1)
+   browserVersion=browserVersion.substring(0,tempVersion);
+
+return `${browserName} ${browserVersion}`
+
+}
