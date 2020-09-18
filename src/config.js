54cd010169ad0f996a77b3f3e68f175d5244b753
원더 <wonderbotsupport@naver.com>
2020-09-18 23:54:11 +0900

    fix(core): Mixed Content

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
   url:
     "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=http://beta.koreanbots.dev%2Fcallback%2Fdiscord",
 
-  api: "http://api.beta.koreanbots.dev/v2" // Does not end with "/"
+  api: "https://api.beta.koreanbots.dev/v2" // Does not end with "/"
 };
