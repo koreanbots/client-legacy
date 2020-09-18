e0dab29274401c3c734e16176d5b4df6f1cede33
원더 <wonderbotsupport@naver.com>
2020-09-18 21:25:46 +0900

    Feat(Config): For Beta

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   url:
-    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=identify&permissions=0&response_type=code&redirect_uri=https%3A%2F%2Fkoreanbots.dev%2Fcallback",
+    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=http://beta.koreanbots.dev%2Fcallback%2Fdiscord",
 
-  api: "https://api.koreanbots.dev/v1" // Does not end with "/"
+  api: "http://api.beta.koreanbots.dev/v2" // Does not end with "/"
 };
