e79b287846df10648ec32873d1b7e063e9fc5567
원더 <wonderbotsupport@naver.com>
2020-09-19 00:42:39 +0900

    Error catching

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -13,6 +13,6 @@ export default async function( query ) {
     const json = await res.json().catch(e=> { return { message: e } } )
     if(res.status === 401) return window.location.assign(config.url)
     if(res.status.toString().startsWith(4) || res.status.toString().startsWith(5)) return { ...json, code: res.status, message: json.message || '처리중에 문제가 발생하였습니다', }
-    if((json.code && !json.code.toString().startsWith(2)) || json.errors) return { code: 400, message: '처리중에 문제가 발생하였습니다.', errors: json.errors }
+    if((json.code && !json.code.toString().startsWith(2)) || json.errors) return { code: 400, message: json.errors.length > 0 ? json.errors[0].message : '처리중에 문제가 발생하였습니다.', errors: json.errors }
     return { code: res.status, data: json.data }
 }
