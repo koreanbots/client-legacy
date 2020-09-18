a0f483f99b02777cff095d4f852be995489b4a7c
원더 <wonderbotsupport@naver.com>
2020-09-19 00:01:25 +0900

    docs: https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   url:
-    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=http://beta.koreanbots.dev%2Fcallback%2Fdiscord",
+    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=https://beta.koreanbots.dev%2Fcallback%2Fdiscord",
 
   api: "https://api.beta.koreanbots.dev/v2" // Does not end with "/"
 };
