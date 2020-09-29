cc2dd6ac925e4e9636fd91dad2f314308c807898
원더 <wonderbotsupport@naver.com>
2020-09-29 21:52:35 +0900

    Feat(Config): Added Github

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ export default {
   url:
     "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=https://beta.koreanbots.dev%2Fcallback%2Fdiscord",
 
+  github: 'https://github.com/login/oauth/authorize?client_id=Iv1.4f5f2fc92b87b5af&redirect_uri=https://beta.koreanbots.dev/callback/github',
   api: "https://api.beta.koreanbots.dev/v2" // Does not end with "/"
 };
