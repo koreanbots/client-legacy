9b2feb2c9d7fa4e8e760ff5ed36b2fb2876b9f77
원더 <wonderbotsupport@naver.com>
2020-10-02 12:41:17 +0900

    Feat(Redirect): Ignore blank content

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -4,13 +4,19 @@ import { Container } from 'semantic-ui-react'
 const Redirecting = props => {
   if (!props.to) throw new Error('No Link')
   window.location.assign(props.to)
-  if (props.content) return <props.content />
   return (
-    <Container>
-      <div className="loader">
-        <a href={props.to}>자동으로 리다이렉트되지 않는다면 클릭하세요</a>
-      </div>
-    </Container>
+    <>
+      {
+        props.content !== undefined ? props.content
+         : (
+          <Container>
+            <div className="loader">
+              <a href={props.to}>자동으로 리다이렉트되지 않는다면 클릭하세요</a>
+            </div>
+          </Container>
+        )
+      }
+    </>
   )
 }
 
