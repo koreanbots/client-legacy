d5f5a2152093c575e2e7107e55cffa46c78c6871
원더 <wonderbotsupport@naver.com>
2020-10-27 21:40:09 +0900

    Style(graphql): fix warning

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   url:
-    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=https://beta.koreanbots.dev%2Fcallback%2Fdiscord",
+    "https://discord.com/oauth2/authorize?client_id=653534001742741552&scope=email%20identify%20guilds&permissions=0&response_type=code&redirect_uri=http://localhost:3001%2Fcallback%2Fdiscord",
 
-  github: 'https://github.com/login/oauth/authorize?client_id=Iv1.4f5f2fc92b87b5af&redirect_uri=https://beta.koreanbots.dev/callback/github',
-  api: "https://api.beta.koreanbots.dev/v2" // Does not end with "/"
+  github: "https://github.com/login/oauth/authorize?client_id=Iv1.4f5f2fc92b87b5af&redirect_uri=http://localhost:3001/callback/github",
+  api: "http://localhost:3000/v2" // Does not end with "/"
 };
